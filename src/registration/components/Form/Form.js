f98ecf176ef84364e1e58020c3c0a4b38360f12c
AnshulJS <anshul.nautiyal@ril.com>
2020-06-07 00:12:39 +0530

    Add class drop down list

--- src/registration/components/Form/Form.js
+++ src/registration/components/Form/Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export const FormInput = (props) => {
   const {
@@ -12,6 +12,7 @@ export const FormInput = (props) => {
     id = "",
     reset = false,
   } = props;
+  const dropDown = useRef();
 
   const [userInput, setUserInput] = useState("");
 
@@ -23,22 +24,73 @@ export const FormInput = (props) => {
     setUserInput("");
   }, [reset]);
 
-  return (
-    <div className="inputContainer">
-      <label htmlFor={id}>
-        {label}
-        <sup>*</sup>
-      </label>
-      <input
-        type={type}
-        name={name}
-        value={userInput}
-        autoFocus={autofocus}
-        placeholder={placeholder}
-        required={required}
-        id={id}
-        onChange={handleUserInput}
-      />
-    </div>
-  );
+  const handleDropDown = () => {
+    dropDown.current.style.display = "block";
+  };
+  const selectClass = (standard) => () => {
+    dropDown.current.style.display = "none";
+    document.getElementById("form_class").value = standard;
+  };
+
+  const createFormDom = () => {
+    const {
+      type = "",
+      label = "",
+      placeholder = "",
+      name = "",
+      autofocus = false,
+      required = true,
+      id = "",
+      reset = false,
+    } = props;
+
+    if (type === "dropDown") {
+      return (
+        <div className="dropDown">
+          <label htmlFor={id}>
+            {label}
+            <sup>*</sup>
+          </label>
+          <input
+            type={type}
+            name={name}
+            value={userInput}
+            autoFocus={autofocus}
+            placeholder={placeholder}
+            required={required}
+            id={id}
+            onChange={handleUserInput}
+          />
+          <div className="hidden" onClick={handleDropDown}></div>
+          <div className="list" ref={dropDown}>
+            <div onClick={selectClass("9th")}>9th</div>
+            <div onClick={selectClass("10th")}>10th</div>
+            <div onClick={selectClass("11th")}>11th</div>
+            <div onClick={selectClass("12th")}>12th</div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <label htmlFor={id}>
+            {label}
+            <sup>*</sup>
+          </label>
+          <input
+            type={type}
+            name={name}
+            value={userInput}
+            autoFocus={autofocus}
+            placeholder={placeholder}
+            required={required}
+            id={id}
+            onChange={handleUserInput}
+          />
+        </>
+      );
+    }
+  };
+
+  return <div className="inputContainer">{createFormDom()}</div>;
 };
